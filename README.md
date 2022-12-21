0d434e6824aac4ecb581c085217d549d617c8273
Grzegorz Zioło <grzegorz.ziolo@gmail.com>
2022-12-21 12:44:56 +0100

    bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # RPA_SAP
-Python module delivers some actions to manipulate with PDF files.
+Python module delivers some actions to automate SAPGUI operations (Sap Scripting)
 The module is compatibile with the Robocorp.
 
 ## Installation
@@ -10,55 +10,14 @@ pip install rpa-sap
 ```
 
 ## Example
-### Generating pdf file from text
+### Open new SAPGUI session
 ```
-from rpa_pdf import Pdf
+from rpa_sap import SapGui
 
-pdf = Pdf()
+sapgui = SapGui()
 
-pdf.text_to_pdf('some text', 'c:/temp/somefile.pdf)
-```
-### Compress pdf file
-```
-from rpa_pdf import Pdf
-
-pdf = Pdf()
-
-pdf.compress('c:/temp/somefile.pdf')
-```
-### Add Code39 stamp
-```
-from rpa_pdf import Pdf
-
-pdf = Pdf()
-
-pdf.add_code39_stamp('c:/temp/input_file.pdf', 'c:/temp/output_file.pdf', '12345678')
-```
-### Merging pdf files
-```
-from rpa_pdf import Pdf
-
-pdf = Pdf()
-
-pdf.merge(['c:/temp/file1.pdf', 'c:/temp/file2.pdf'], 'c:/temp/merged.pdf')
-```
-### Add text stamp
-```
-from rpa_pdf import Pdf
-
-pdf = Pdf()
-
-pdf.add_text_stamp('c:/temp/input_file.pdf', 'c:/temp/output_file.pdf', 'some text')
-```
-### Print PDF document on the printer
-```
-from rpa_pdf import Pdf
-
-pdf = Pdf()
-
-pdf.print('c:/temp/document.pdf', 'printer1')
+sapgui.open_new_session(connection_string, user_id, password, client, language)
 ```
 
 ### Dependencies
-Python packages: PyPDF2 >= 1.28.5, fpdf2 >= 2.5.6, python-barcode >= 0.14.0
-External dependencies: DejaVu font, SumatraPdf.exe (both included in the package)
+Python packages: pandas >= 1.4.4, pywin32 >= 303
--- tests/test_sapgui.py
+++ tests/test_sapgui.py
@@ -4,17 +4,12 @@ import json
 from rpa_sap import SapGui
 
 SAPGUI: SapGui = SapGui()
-SECRETS = json.load('credentials.json')
+with open('tests/credentials.json') as data:
+    SECRETS = json.load(data)
 
 class TestSapGui(TestCase):
     def test_open_and_close_new_session(self):
-        test_value = SAPGUI.open_new_session(
-            connection_string=SECRETS['connection_string'],
-            user_id=SECRETS['user_id'],
-            password=SECRETS['password'],
-            client=SECRETS['client'],
-            language=SECRETS['language']
-        )
+        test_value = SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
         excpected_value = True
 
         SAPGUI.close_session()
@@ -23,5 +18,62 @@ class TestSapGui(TestCase):
         self.assertEqual(test_value, excpected_value)
 
     def test_activate_session(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
         SAPGUI.activate_session(0,0)
+        self.assertEqual(SAPGUI.get_session_index(), 0)
+        self.assertEqual(SAPGUI.get_connection_index(), 0)
+
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
         SAPGUI.activate_session()
+        self.assertEqual(SAPGUI.get_session_index(), 0)
+        self.assertEqual(SAPGUI.get_connection_index(), 1)
+
+        SAPGUI.close_all_sessions()
+        SAPGUI.close_sap_logon()
+
+    def test_check_if_session_exists(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        self.assertEqual(SAPGUI.check_if_session_exists(), True)
+        self.assertEqual(SAPGUI.check_if_session_exists(0), True)
+        self.assertEqual(SAPGUI.check_if_session_exists(1), False)
+        SAPGUI.close_session()
+        SAPGUI.close_sap_logon()
+
+        self.assertEqual(SAPGUI.check_if_session_exists(), False)
+        self.assertEqual(SAPGUI.check_if_session_exists(0), False)
+        
+    def test_get_session_info(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        session_info = SAPGUI.get_session_info()
+        self.assertEqual(session_info['connection index'], '/app/con[0]')
+        print(session_info)
+        SAPGUI.close_session()
+        SAPGUI.close_sap_logon()
+
+    def test_count_connections(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        self.assertEqual(SAPGUI.count_connections(), 1)
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        self.assertEqual(SAPGUI.count_connections(), 2)
+        SAPGUI.close_all_sessions()
+        SAPGUI.close_sap_logon()
+
+    def test_count_sessions(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        self.assertEqual(SAPGUI.count_sessions(), 1)
+        SAPGUI.close_session()
+        SAPGUI.close_sap_logon()
+
+    def test_is_session_busy(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        self.assertEqual(SAPGUI.is_session_busy(), False)
+        SAPGUI.close_session()
+        SAPGUI.close_sap_logon()
+
+    def test_is_active(self):
+        SAPGUI.open_new_session(SECRETS['connection_string'], SECRETS['user_id'], SECRETS['password'], SECRETS['client'], SECRETS['language'])
+        self.assertEqual(SAPGUI.is_session_active(), True)
+        SAPGUI.close_session()
+        SAPGUI.close_sap_logon()
+
+    
